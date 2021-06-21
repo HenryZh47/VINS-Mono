3f0ab2e217cbd23616f080637af6de84f2f263fa
Henry Zhang <hzhang0407@gmail.com>
2021-06-21 15:30:16 -0400

    add dump function to script

--- scripts/depth_filter/visualize_depth_filter_log.py
+++ scripts/depth_filter/visualize_depth_filter_log.py
@@ -2,8 +2,8 @@
 
 '''
 Visualize VINS depth filter log
-original: time1:point_id1,mu1,sigma1;point_id2,mu2,sigma2;...
-          time2:point_id1,mu1,sigma1;point_id3,mu3,sigma3;...
+original: time1:point_id1,mu1,sigma1,inlier_ratio;point_id2,mu2,sigma2,inlier_ratio;...
+          time2:point_id1,mu1,sigma1,inlier_ratio;point_id3,mu3,sigma3,inlier_ratio;...
 '''
 
 from functools import total_ordering
@@ -11,14 +11,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 from scipy.stats import norm
+import os
+import gc
 
 
 def main():
     parser = argparse.ArgumentParser(
         description='Visualize VINS depth filter log')
     parser.add_argument('log_path', type=str, help='path to log file')
-    parser.add_argument('point_itr_thresh', type=int,
+    parser.add_argument('--point_itr_thresh', type=int, default=200,
                         help='threshold of minimum number of iteration for a point to be visualized')
+    parser.add_argument('--skip_frame', type=int, default=0,
+                        help='skip x number of frames when visualize to reduce memory usage')
+    parser.add_argument('--dump_file_path', type=str, default="",
+                        help='foler to save all point plots and result')
     args = parser.parse_args()
 
     # parse file and create a dictionary of points
@@ -59,20 +65,40 @@ def main():
         point_iters = df_log[point_id]
         iter_num = 0
         total_iter_num = len(point_iters)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
         for point_iter in point_iters:
+            if ((iter_num % args.skip_frame) != 0):
+                iter_num += 1
+                continue
             # get lowerbound and upperbound for visualization
             mu = point_iter[0]
             sigma = point_iter[1]
             lb = max(0, mu - 3*sigma)
             ub = mu + 3*sigma
             x_axis = np.arange(lb, ub, 0.00001)
-            curve_color = (1-(iter_num/total_iter_num), iter_num/total_iter_num, 0)
-            
-            plt.plot(x_axis, norm.pdf(x_axis, mu, sigma), color=curve_color)
+            curve_color = (1-(iter_num/total_iter_num),
+                           iter_num/total_iter_num, 0)
+
+            ax.plot(x_axis, norm.pdf(x_axis, mu, sigma), color=curve_color)
             iter_num += 1
+        
+        # put inlier ratio on fig
+        ax.text(0.9, 0.9, str(point_iter[2]), ha='center', va='center', transform=ax.transAxes)
 
         print("inlier ratio:", point_iter[2])
-        plt.show()
+        # show plot if not dumping to file
+        if (args.dump_file_path == ""):
+            fig.show()
+        else:
+            # save plot to file
+            plt_path = os.path.join(
+                args.dump_file_path, (str(point_id) + '.png'))
+            fig.savefig(plt_path)
+            print("saved fig to:", plt_path)
+        plt.close(fig)
+
 
 if __name__ == "__main__":
     main()
